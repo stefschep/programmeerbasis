18572239d00aa40347789d7856ce1c84e48d1c41
Stef Schep <97091233@st.deltion.nl>
2022-09-29 11:27:47 +0200

    rekenen1

---
+++ rekenen.php
@@ -0,0 +1,22 @@
+<?php
+$getal1=15;
+$getal2=3;
+$som=$getal1 + $getal2;
+echo "$getal1 + $getal2 = ".$som;
+echo "\n";
+$verschil=$getal1 - $getal2;
+echo "$getal1 - $getal2 = ".$verschil;
+echo "\n";
+
+$product=$getal1 * $getal2;
+echo "$getal1 * $getal2 = ".$product;
+echo "\n";
+
+$quotient=$getal1 / $getal2;
+echo "$getal1 / $getal2 = ".$quotient;
+echo "\n";
+
+$rest=$getal1 % $getal2;
+echo "$getal1 % $getal2 = ".$rest;
+echo "\n";
+?>
