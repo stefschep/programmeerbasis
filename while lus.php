527e62fd65013ac2aac6227fd5c4d7e996af16d9
Stef Schep <97091233@st.deltion.nl>
2022-09-29 11:51:48 +0200

    while lus.php

---
+++ while lus.php
@@ -0,0 +1,15 @@
+<?php
+
+$teller=1;
+$maximum=10;
+
+echo "Teller is voor de while lus ".$teller."\n";
+
+while ( $teller <= $maximum ) {
+    echo "Teller is nu ".$teller."\n";
+$teller++;
+}
+
+echo "Teller is na de while lus ".$teller."\n";
+
+?>
