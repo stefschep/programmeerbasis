7803ecfe50875a8bc5060b7961431f3b541ab02c
Stef Schep <97091233@st.deltion.nl>
2022-09-29 12:22:09 +0200

    hello world.php

--- hello world.php
+++ hello world.php
@@ -1,10 +1,10 @@
 <?php
 
 $teller = 1;
-$maximum = 10;
+$maximum = 1000;
 $tekst =  "Hello world!\n" ;
 while ($teller <= $maximum) {
-    echo $tekst;
+    echo $tekst . $teller;
 
     $teller++;
 }
